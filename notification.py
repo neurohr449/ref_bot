3e1c4591869617e74b7a659b0e5b221682e15a42
selivants <selivants@gmail.com>
2025-07-08 02:03:20 +0300

    Update notification.py

--- notification.py
+++ notification.py
@@ -73,7 +73,7 @@ async def check_for_status_updates(bot: Bot, pool, sheet_id: str):
         async with pool.acquire() as conn:
             async with conn.transaction():  # Транзакция для атомарности
                 for lead in leads:
-                    referral_id = str(lead["id Реферала"])
+                    referral_id = str(int(float(lead["id Реферала"])))
                     current_status = str(lead["Статус"])
                     
                     # Получаем данные из БД
